b9b8f700153c67330720a3ba27b0ffb55d3194f0
Nuwan <nuwan_g@live.com>
2023-03-16 16:48:39 +0530

    Clean up code in darkModeSwitch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,9 @@ import Layout from '../components/Layout';
 type Props = {};
 
 export default function Index({ }: Props): JSX.Element {
-
   return (
     <Layout className="flex items-center justify-center my-40">
       <h1 className="text-9xl text-center">Hello World</h1>
     </Layout>
-
   );
 }
