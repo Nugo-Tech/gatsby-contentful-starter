a683efaad4bd380c9a242b23c4393e768d8e2dea
Nuwan <nuwan_g@live.com>
2023-03-16 20:47:19 +0530

    Resolve bug in gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { ThemeProvider } from '@mui/material/styles'
 import store from './src/state/store'
-import theme from './src/theme'
 import './src/styles/global.scss'
 
 export const wrapRootElement = ({ element }) => {
-  return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        {element}
-      </ThemeProvider>
-    </Provider>
-  )
+  return <Provider store={store}>{element}</Provider>
 }
